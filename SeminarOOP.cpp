f567006f5c0ee90736b08b9723bceed5fd7d9dd0
Jeworrek Oliver <hsvdgm0@gmail.com>
2025-03-06 19:56:17 +0200

    added main

--- SeminarOOP.cpp
+++ SeminarOOP.cpp
@@ -1,4 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Dog {
+  public:
+  bool eat() {
+    cout<<"a mancat!\n";
+    return true;
+  };
+
+  int get_age() {
+    return age;
+  }
+  int set_age(int new_age) {
+    age = new_age;
+    return age;
+  }
+private:
+  int age;
+};
 
 int main(){
+  Dog dog;
+  dog.set_age(10);
+  cout<<dog.get_age();
+  // dog.eat();
+  // Dog dog1 = dog;
   return 0;
 }
